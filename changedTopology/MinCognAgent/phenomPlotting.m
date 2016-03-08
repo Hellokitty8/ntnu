33f896add06858f207d0da9386a985df1768b7b7
thomasalm <thomas_almenningen@hotmail.com>
2016-03-08 19:54:02 +0100

    fix..

--- changedTopology/MinCognAgent/phenomPlotting.m
+++
@@ -1,8 +0,0 @@
-data = importdata('phenoms.txt');
-
-subplot(2,1,1);
-line(data(:, 1), data(:, 2), 'LineWidth', 2);
-title('Generation number of perfect solution found(200 == not found)');
-subplot(2,1,2);
-line(data(:, 1), data(:, 3), 'LineWidth', 2);
-title('Max Fitness Found (80 == perfect)');
